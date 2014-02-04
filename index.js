45dec2e82b8c2530b71b3013036066920c31296e
cyberfunk <hello@cyberfunk.org>
2014-02-05 04:13:10 +0800

    Migrate native functionality, closes #4.

--- index.js
+++ index.js
@@ -141,38 +141,9 @@ function merge(source, target, filter) {
   return target;
 }
 
-var native = {
-  to: function(str, delimiter, json) {
-    if(typeof(str) != 'string') return str;
-    if(str === 'true') return true;
-    if(str === 'false') return false;
-    if(str === 'null') return null;
-    if(str === 'undefined') return undefined;
-    var num = Number(str);
-    if(!isNaN(num)) return num;
-    if(str && json && /^["{\[]/.test(str)) {
-      return JSON.parse(str);
-    }
-    if(str && delimiter && ~str.indexOf(delimiter)) {
-      var arr = str.split(delimiter);
-      for(var i = 0;i < arr.length;i++) {
-        arr[i] = native.to(arr[i], delimiter, json);
-      }
-      return arr;
-    }
-    return str;
-  },
-  from: function(val, delimiter, json) {
-    if(delimiter && Array.isArray(val)) return val.join(delimiter);
-    if(json && val && (typeof val == 'object')) return JSON.stringify(val);
-    return '' + val;
-  }
-}
-
 module.exports.repeat = repeat;
 module.exports.pad = pad;
 module.exports.camelcase = camelcase;
 module.exports.delimited = delimited;
 module.exports.home = home;
 module.exports.merge = merge;
-module.exports.native = native;
