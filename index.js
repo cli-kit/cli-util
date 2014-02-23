2ebe7782b5626dbe84d61ca4270bf540ccfef331
cyberfunk <hello@cyberfunk.org>
2014-02-23 22:40:30 +0800

    Handle hard line breaks in wrap().
    Also be more strict about accepting NaN values for column and amount as
    it normally indicates addition of undefined variables.
    
    Add first option to wrap() to that the first line may also be indented.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var util = require('util');
+var eol = require('os').EOL;
 
 function ltrim(str) {
   return str.replace(/^\s+/, '');
@@ -10,46 +11,74 @@ function ltrim(str) {
  *  @param str The string to wrap.
  *  @param col The column to indent lines (first line is not indented).
  *  @param amount The amount to wrap at.
+ *  @param first Also pad first line if column is greater than zero.
  */
-function wrap(str, col, amount) {
-  amount = amount || 80;
-  amount = Math.max(amount, 2);
-  var over = str.length + col > amount;
-  var target = amount - col, parts = [];
-  var padding = repeat(col);
-  var words = str.split(' '), line = '', word;
-  function append(line) {
-    parts.push(
-      parts.length ? (col > 0 ? padding : '') + ltrim(line) : line);
+function wrap(str, col, amount, first) {
+  if(!str || typeof str !== 'string') return str;
+  if(isNaN(col) || isNaN(amount)) {
+    throw new TypeError('Wrap cannot operate on NaN ' +
+      (isNaN(col) ? 'column' : 'amount'));
   }
-  if(over) {
-    var i, l = words.length;
-    for(i = 0;i < l;i++) {
-      word = words[i];
-      // hyphenate long words that exceed
-      // the target length limit
-      if(word.length >= target) {
-        if(word.length === target) {
-          append(word);
+  amount = parseInt(amount) || 80;
+  amount = Math.max(amount, 2);
+  col = parseInt(col) || 0;
+  col = Math.max(col, 0);
+  var padding = repeat(col);
+  var target = amount - col;
+  function block(str) {
+    var over = str.length + col > amount;
+    var parts = [];
+    var words = str.split(' '), line = '', word;
+    function append(line) {
+      parts.push(
+        parts.length ? (col > 0 ? padding : '') + ltrim(line) : line);
+    }
+    if(over) {
+      var i, l = words.length;
+      for(i = 0;i < l;i++) {
+        word = words[i];
+        // hyphenate long words that exceed
+        // the target length limit
+        if(word.length >= target) {
+          if(word.length === target) {
+            append(word);
+          }else{
+            line = word.substr(0, target - 1) + '-';
+            words.splice(i + 1, 0, word.substr(target - 1));
+            append(line);
+            line = '';
+            l++;
+          }
+          continue;
+        }
+        if((line + ' ' + word).length <= target) {
+          line += line === '' ? word : ' ' + word;
         }else{
-          line = word.substr(0, target - 1) + '-';
-          words.splice(i + 1, 0, word.substr(target - 1));
           append(line);
           line = '';
-          l++;
+          i--;
         }
-        continue;
       }
-      if((line + ' ' + word).length <= target) {
-        line += line === '' ? word : ' ' + word;
-      }else{
-        append(line);
-        line = '';
-        i--;
-      }
+      if(line !== '') append(line);
+      return parts.join(eol).trim();
     }
-    if(line !== '') append(line);
-    return parts.join('\n').trim();
+    return !first ? str : padding + str;
+  }
+  var re = /\n+/, parts, past;
+  if(!re.test(str)) {
+    str = block(str);
+  }else{
+    // respect hard line breaks
+    parts = str.split(eol);
+    parts = parts.map(function(segment) {
+      // empty string is multiple consecutive newlines
+      if(!segment) return segment;
+      segment = block(segment);
+      if(past && col > 0) segment = padding + segment;
+      past = true;
+      return segment;
+    })
+    str = parts.join(eol);
   }
   return str;
 }
--- test/unit/wrap.js
+++ test/unit/wrap.js
@@ -2,10 +2,46 @@ var expect = require('chai').expect;
 var util = require('../..');
 
 describe('cli-util:', function() {
+  it('should ignore non-string', function(done) {
+    var res = util.wrap(null, 0, 80);
+    expect(res).to.eql(null);
+    done();
+  });
+  it('should throw type error on invalid column', function(done) {
+    var str = 'I have a dream';
+    function fn() {
+      util.wrap(str, NaN);
+    }
+    expect(fn).throws(TypeError);
+    done();
+  });
+  it('should throw type error on invalid amount', function(done) {
+    var str = 'I have a dream';
+    function fn() {
+      util.wrap(str, 0, 'value');
+    }
+    expect(fn).throws(TypeError);
+    done();
+  });
   it('should leave short string untouched', function(done) {
     var str = 'I have a dream';
-    var res = util.wrap(str, 0);
+    var res = util.wrap(str, 0, 80);
     var expected = 'I have a dream';
+    expect(res).to.eql(expected);
+    done();
+  });
+  it('should use default amount', function(done) {
+    var str = 'I have a dream';
+    var res = util.wrap(str, 0, 0);
+    var expected = 'I have a dream';
+    expect(res).to.eql(expected);
+    done();
+  });
+  it('should indent single line (first)', function(done) {
+    var str = 'I have a dream';
+    var padding = util.repeat(20);
+    var res = util.wrap(str, 20, 80, true);
+    var expected = padding + 'I have a dream';
     expect(res).to.eql(expected);
     done();
   });
@@ -65,4 +101,42 @@ describe('cli-util:', function() {
       done();
     }
   );
+  it('should respect hard line breaks', function(done) {
+    var str = 'I have a dream.\n\nThat one day this nation will rise up and ';
+    str += 'live out the true meaning of its creed: "We hold these truths '
+    str += 'to be self-evident, that all men are created equal."';
+    var res = util.wrap(str, 0, 80);
+    var expected = 'I have a dream.\n\nThat one day this nation will rise up ';
+    expected += 'and live out the true meaning of its\ncreed: "We hold these ';
+    expected += 'truths to be self-evident, that all men are created\nequal."';
+    expect(res).to.eql(expected);
+    done();
+  });
+
+  it('should respect indent with hard line breaks', function(done) {
+    var str = 'I have a dream.\n\nThat one day this nation will rise up and ';
+    str += 'live out the true meaning of its creed: "We hold these truths '
+    str += 'to be self-evident, that all men are created equal."';
+    var res = util.wrap(str, 20, 80);
+    var expected = 'I have a dream.\n\n                    That one day this ';
+    expected += 'nation will rise up and live out the true\n';
+    expected += '                    meaning of its creed: "We hold these '
+    expected += 'truths to be\n                    self-evident, that all ';
+    expected += 'men are created equal."';
+    expect(res).to.eql(expected);
+    done();
+  });
+  it('should indent first with hard line breaks', function(done) {
+    var str = 'I have a dream.\n\nThat one day this nation will rise up and ';
+    str += 'live out the true meaning of its creed: "We hold these truths '
+    str += 'to be self-evident, that all men are created equal."';
+    var res = util.wrap(str, 20, 80, true);
+    var expected = '                    I have a dream.\n\n                    ';
+    expected += 'That one day this nation will rise up and live out the true\n';
+    expected += '                    meaning of its creed: "We hold these ';
+    expected += 'truths to be\n                    self-evident, that all men ';
+    expected += 'are created equal."';
+    expect(res).to.eql(expected);
+    done();
+  });
 })
